8ab7d959300d39d4e77331ec91fb175bdcd8188a
Aakash <innateblogger@gmail.com>
2015-12-31 19:12:03 +0530

    Basic Product Listing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-System.register(['angular2/platform/browser', 'angular2/core'], function(exports_1) {
+System.register(['angular2/core', 'angular2/platform/browser'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,41 +8,145 @@ System.register(['angular2/platform/browser', 'angular2/core'], function(exports
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var browser_1, core_1;
-    var Product, InventoryApp;
+    var core_1, browser_1;
+    var Product, ProductImage, ProductDepartment, PriceDisplay, ProductRow, ProductsList, InventoryApp;
     return {
         setters:[
-            function (browser_1_1) {
-                browser_1 = browser_1_1;
-            },
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
             }],
         execute: function() {
+            /**
+             * Components List
+                 Inventory App
+                   - ProductsList
+                     - ProductRow
+                        - productimage
+                        - productdept
+                        - pricedisplay
+             */
+            /**
+             * Provides a `Product` object
+             */
             Product = (function () {
-                function Product(product_sku, product_name, product_url, product_dept, product_price) {
-                    this.sku = product_sku;
-                    this.name = product_name;
-                    this.imageUrl = product_url;
-                    this.department = product_dept;
-                    this.price = product_price;
+                function Product(sku, name, imageUrl, department, price) {
+                    this.sku = sku;
+                    this.name = name;
+                    this.imageUrl = imageUrl;
+                    this.department = department;
+                    this.price = price;
                 }
                 return Product;
             })();
+            /**
+             * @ProductImage: A component to show a single Product's image
+             */
+            ProductImage = (function () {
+                function ProductImage() {
+                }
+                ProductImage = __decorate([
+                    core_1.Component({
+                        selector: 'product-image',
+                        host: { class: 'ui small image' },
+                        inputs: ['product'],
+                        template: "\n  <img class=\"product-image\" [src]=\"product.imageUrl\">\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductImage);
+                return ProductImage;
+            })();
+            /**
+             * @ProductDepartment: A component to show the breadcrumbs to a
+             * Product's department
+             */
+            ProductDepartment = (function () {
+                function ProductDepartment() {
+                }
+                ProductDepartment = __decorate([
+                    core_1.Component({
+                        selector: 'product-department',
+                        inputs: ['product'],
+                        template: "\n  <div class=\"product-department\">\n    <span *ngFor=\"#name of product.department; #i=index\">\n      <a href=\"#\">{{ name }}</a>\n      <span>{{i < (product.department.length-1) ? '>' : ''}}</span>\n    </span>\n  </div>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductDepartment);
+                return ProductDepartment;
+            })();
+            /**
+             * @PriceDisplay: A component to show the price of a
+             * Product
+             */
+            PriceDisplay = (function () {
+                function PriceDisplay() {
+                }
+                PriceDisplay = __decorate([
+                    core_1.Component({
+                        selector: 'price-display',
+                        inputs: ['price'],
+                        template: "\n  <div class=\"price-display\">${{ price }}</div>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], PriceDisplay);
+                return PriceDisplay;
+            })();
+            /**
+             * @ProductRow: A component for the view of single Product
+             */
+            ProductRow = (function () {
+                function ProductRow() {
+                }
+                ProductRow = __decorate([
+                    core_1.Component({
+                        selector: 'product-row',
+                        inputs: ['product'],
+                        host: { 'class': 'item' },
+                        templateUrl: 'partials/product-row.html',
+                        directives: [ProductImage, ProductDepartment, PriceDisplay],
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductRow);
+                return ProductRow;
+            })();
+            /**
+             * @ProductsList: A component for rendering all ProductRows and
+             * storing the currently selected Product
+             */
+            ProductsList = (function () {
+                function ProductsList() {
+                }
+                ProductsList = __decorate([
+                    core_1.Component({
+                        selector: 'products-list',
+                        inputs: ['productList'],
+                        templateUrl: 'partials/product-list.html',
+                        directives: [ProductRow]
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductsList);
+                return ProductsList;
+            })();
+            /**
+             * @InventoryApp: the top-level component for our application
+             */
             InventoryApp = (function () {
                 function InventoryApp() {
-                    var newProduct = [
-                        new Product('avcf1243', 'Adiddas Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 220.00),
-                        new Product('avcf1sdsd3', 'Nike Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 250.00),
-                        new Product('avcf1243sdsds', 'Fila Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 260.00),
-                        new Product('avcf124sdsdsd3', 'Puma Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 280.00),
+                    this.products = [
+                        new Product('MYSHOES', 'Black Running Shoes', '/resources/images/products/black-shoes.jpg', ['Men', 'Shoes', 'Running Shoes'], 109.99),
+                        new Product('NEATOJACKET', 'Blue Jacket', '/resources/images/products/blue-jacket.jpg', ['Women', 'Apparel', 'Jackets & Vests'], 238.99),
+                        new Product('NICEHAT', 'A Nice Black Hat', '/resources/images/products/black-hat.jpg', ['Men', 'Accessories', 'Hats'], 29.99)
                     ];
-                    this.product = newProduct;
                 }
+                InventoryApp.prototype.productWasSelected = function (product) {
+                    console.log('Details of the product Clicked', product);
+                };
                 InventoryApp = __decorate([
                     core_1.Component({
                         selector: 'inventory-app',
-                        templateUrl: 'partials/shell.html'
+                        templateUrl: 'partials/inventory-app.html',
+                        directives: [ProductsList]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], InventoryApp);
--- app.ts
+++ app.ts
@@ -1,51 +1,147 @@
-import { bootstrap } from 'angular2/platform/browser';
-import { Component } from 'angular2/core';
+import {
+Component,
+EventEmitter
+} from 'angular2/core';
 
-class Product{
+import {bootstrap} from 'angular2/platform/browser';
 
-  public sku: string;
+/**
+ * Components List
+     Inventory App
+       - ProductsList
+         - ProductRow
+            - productimage
+            - productdept
+            - pricedisplay
+ */
 
-  public name: string;
-
-  public imageUrl: string;
-
-  public department: string[];
-
-  public price: number; 
-
-  constructor( product_sku: string, product_name: string, product_url: string, product_dept: string[], product_price: number) {
-     this.sku          = product_sku;
-     this.name         = product_name;
-     this.imageUrl     = product_url;
-     this.department   = product_dept;
-     this.price        = product_price
-   }
-
-}
-
-
-@Component({
-  selector: 'inventory-app',
-  templateUrl : 'partials/shell.html'
-})
-class InventoryApp{
-
-  product: Product[];
-
-  constructor() {
-
-    let newProduct: Product[] = [
-
-      new Product('avcf1243', 'Adiddas Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 220.00),
-      new Product('avcf1sdsd3', 'Nike Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 250.00),
-      new Product('avcf1243sdsds', 'Fila Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 260.00),
-      new Product('avcf124sdsdsd3', 'Puma Air', 'http://lorempixel/200/300', ['Men', 'Accessories', 'Shoes'], 280.00),
-
-      ]
-
-    this.product = newProduct;
-
+/**
+ * Provides a `Product` object
+ */
+class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number) {
   }
-
 }
-bootstrap(InventoryApp);
+
+/**
+ * @ProductImage: A component to show a single Product's image 
+ */
+@Component({
+  selector: 'product-image',
+  host: { class: 'ui small image' },
+  inputs: ['product'],
+  template: `
+  <img class="product-image" [src]="product.imageUrl">
+  `
+})
+class ProductImage {
+  product: Product;
+}
+
+/**
+ * @ProductDepartment: A component to show the breadcrumbs to a 
+ * Product's department
+ */
+@Component({
+  selector: 'product-department',
+  inputs: ['product'],
+  template: `
+  <div class="product-department">
+    <span *ngFor="#name of product.department; #i=index">
+      <a href="#">{{ name }}</a>
+      <span>{{i < (product.department.length-1) ? '>' : ''}}</span>
+    </span>
+  </div>
+  `
+})
+class ProductDepartment {
+  product: Product;
+}
+
+/**
+ * @PriceDisplay: A component to show the price of a 
+ * Product
+ */
+@Component({
+  selector: 'price-display',
+  inputs: ['price'],
+  template: `
+  <div class="price-display">\${{ price }}</div>
+  `
+})
+class PriceDisplay {
+  price: number;
+}
+
+/**
+ * @ProductRow: A component for the view of single Product
+ */
+@Component({
+  selector: 'product-row',
+  inputs: ['product'],
+  host: { 'class': 'item' },
+  templateUrl: 'partials/product-row.html',
+  directives: [ProductImage, ProductDepartment, PriceDisplay],
+})
+class ProductRow{
+  product: Product;
+}
+
+
+
+/**
+ * @ProductsList: A component for rendering all ProductRows and 
+ * storing the currently selected Product
+ */
+@Component({
+  selector: 'products-list',
+  inputs: ['productList'],
+  templateUrl: 'partials/product-list.html',
+  directives: [ProductRow]
+})
+class ProductsList{
+
+  productList: Product;  // productList is of Product Type
+  
+}
+
+
+/**
+ * @InventoryApp: the top-level component for our application
+ */
+ @Component({
+   selector : 'inventory-app',
+   templateUrl: 'partials/inventory-app.html',
+   directives: [ProductsList]
+ })
+ class InventoryApp{
+   products: Product[];
+   constructor(){
+     this.products = [
+       new Product(
+         'MYSHOES', 'Black Running Shoes',
+         '/resources/images/products/black-shoes.jpg',
+         ['Men', 'Shoes', 'Running Shoes'],
+         109.99),
+       new Product(
+       'NEATOJACKET', 'Blue Jacket',
+       '/resources/images/products/blue-jacket.jpg',
+       ['Women', 'Apparel', 'Jackets & Vests'],
+       238.99),
+     new Product(
+       'NICEHAT', 'A Nice Black Hat',
+       '/resources/images/products/black-hat.jpg',
+       ['Men', 'Accessories', 'Hats'],
+       29.99)
+     ]
+   }
+   productWasSelected(product: Product): void {
+     console.log('Details of the product Clicked', product);
+   }
+ }
+ bootstrap(InventoryApp);
